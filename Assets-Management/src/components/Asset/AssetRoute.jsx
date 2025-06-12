5a31b9c73ef0403c09f1925bb6c4d2ce6682ca61
bittu <bittu.kumar@vservit.com>
2025-06-12 16:26:43 +0530

    create new asset-import component and setup routing for this component

--- Assets-Management/src/components/Asset/AssetRoute.jsx
+++ Assets-Management/src/components/Asset/AssetRoute.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import AddFixedAssets from './Global/AddFixedAssets';
+import AddFixedAssets from "./Global/AddFixedAssets";
+import AssetImport from "./Global/AssetImport";
 const AssetRoute = () => {
   return (
-      <Routes>
-        <Route path="AddFixedAssets" element={<AddFixedAssets />} />
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="AddFixedAssets" element={<AddFixedAssets />} />
+      <Route path="asset-import" element={<AssetImport />} />
+    </Routes>
+  );
+};
 
-export default AssetRoute
+export default AssetRoute;

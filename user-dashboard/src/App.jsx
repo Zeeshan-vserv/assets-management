e3986de04e9cd2915654b2a7dcc18c167c4f595a
Zeeshan Ahmed <zeeshan.ahmed@vservit.com>
2025-07-03 11:14:03 +0530

    fetching the code

--- user-dashboard/src/App.jsx
+++ user-dashboard/src/App.jsx
@@ -3,10 +3,9 @@ import Main from "./components/Main.jsx";
 import Login from "./components/Login.jsx";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
-// import DashboardAsset from "./components/Dashboard/DashboardAsset.jsx";
 function App() {
   const user = useSelector((state) => state.authReducer.authData);
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   return (
     <>
       <Routes>

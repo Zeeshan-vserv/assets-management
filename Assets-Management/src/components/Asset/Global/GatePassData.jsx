db530a6a6aa9d066553d510813fa8a562ea5a889
bittu <bittu.kumar@vservit.com>
2025-07-28 18:15:05 +0530

    add dropdown select ticket type at service request of end user dashboard

--- Assets-Management/src/components/Asset/Global/GatePassData.jsx
+++ Assets-Management/src/components/Asset/Global/GatePassData.jsx
@@ -436,7 +436,7 @@ function GatePassData() {
   return (
     <>
       <div className="flex flex-col w-[100%] min-h-full  p-4 bg-slate-100">
-        <h2 className="text-lg font-semibold mb-6 text-start">GET PAS</h2>
+        <h2 className="text-lg font-semibold mb-6 text-start">GET PASS</h2>
         <MaterialReactTable table={table} />
         {openGetPassDeleteModal && (
           <>

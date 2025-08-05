3f1ed9779446999ced53f2ac7f777a7e19107d49
bittu <bittu.kumar@vservit.com>
2025-08-05 18:16:42 +0530

    fixed ui at admin dashboard

--- Assets-Management/src/components/ConfirmUpdateModal.jsx
+++ Assets-Management/src/components/ConfirmUpdateModal.jsx
@@ -10,13 +10,13 @@ const ConfirmUpdateModal = ({ isOpen, message, onConfirm, onCancel }) => {
         <div className="flex justify-end gap-4">
           <button
             onClick={onConfirm}
-            className="bg-green-600 hover:bg-green-700 text-white font-medium px-5 py-2 rounded-lg shadow-md transition duration-200 ease-in-out hover:scale-[1.02] focus:outline-none focus:ring-2 focus:ring-green-400"
+            className="bg-[#6f7fbc] shadow-[#7a8bca] shadow-md px-4 py-2 rounded-md text-sm text-white transition-all"
           >
             Yes
           </button>
           <button
             onClick={onCancel}
-             className="bg-gray-500 hover:bg-gray-600 text-white font-medium px-5 py-2 rounded-lg shadow-md transition duration-200 ease-in-out hover:scale-[1.02] focus:outline-none focus:ring-2 focus:ring-gray-400"
+             className="bg-[#df656b] shadow-[#F26E75] shadow-md text-white px-4 py-2 rounded-lg transition-all text-sm font-medium"
           >
             Cancel
           </button>
